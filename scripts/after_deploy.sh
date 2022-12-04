c5cf2456f116960ed02db4a41bcf28625a072896
hyeonjun <roo113@naver.com>
2022-12-04 23:02:37 +0900

    cicd pm2 멀티 클러스터 무중단 완성

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #pm2 not found 가 떠서 넣어봄 ...
-#export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 REPOSITORY=/home/ec2-user/cicd-back/
 cd $REPOSITORY
--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #pm2 not found 가 떠서 넣어봄 ...
-#export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 REPOSITORY=/home/ec2-user/cicd-back/
 cd $REPOSITORY
