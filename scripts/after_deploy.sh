5b147ce5e75805b26bac346f0a6363b844dda43a
HyeonJun0626 <roo113@naver.com>
2023-03-01 21:57:08 +0900

    pm2 조건 추가

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -7,5 +7,17 @@ export NVM_DIR="$HOME/.nvm"
 #cd $REPOSITORY
 
 #pm2 start ecosystem.config.js
-pm2 save # 가끔 reload 횟수는 오르지만 변경된 코드가 적용 안되는 문제가 있어서 혹시나 넣어봄..
-pm2 reload ecosystem.config.js
+#pm2 save # 가끔 reload 횟수는 오르지만 변경된 코드가 적용 안되는 문제가 있어서 혹시나 넣어봄..
+#pm2 reload ecosystem.config.js
+
+if ! command -v pm2 &> /dev/null
+then
+    npm install -g pm2
+fi
+
+if pm2 list | grep -q "cicd-back"
+then
+    pm2 reload ecosystem.config.js
+else
+    pm2 start ecosystem.config.js
+fi
